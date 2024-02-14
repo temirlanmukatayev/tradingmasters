118470edd60b8b7afb1a02a5f67290c9cbd24659
Temirlan <temirlanmukatayev@gmail.com>
2024-02-14 23:07:58 +0600

    media settings ready

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     # Django admin
@@ -26,4 +28,4 @@ urlpatterns = [
 
     # Local apps
     path('', include('trades.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
