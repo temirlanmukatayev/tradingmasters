fd9f47f6d55136b0a83a7e25d00e7279b40c4f73
Temirlan <temirlanmukatayev@gmail.com>
2024-03-27 19:25:31 +0500

    Trading Account model tests added

--- trades/tests/test_models.py
+++ trades/tests/test_models.py
@@ -15,25 +15,67 @@ class TradingAccountModelTest(TestCase):
             email='test@mail.com',
             password='testpass123'
         )
-        cls.trading_account = TradingAccount.objects.create(
+        cls.tr_account = TradingAccount.objects.create(
             owner=cls.user,
-            identifier='testidentifier1',
-            title='Test Trading Account',
-            description='Test Trading Account Description',
+            identifier='testidentifier',
+            title='Test Title',
+            description='Test Description',
             type='REAL',
             initial_balance=10000,
             active=True,
-            created_at='2024, 3, 27, 17, 49, 49, 461243',
-            updated_at='2024, 3, 27, 17, 50, 13, 995630'
+            created_at='2024, 3, 27, 18, 59, 23, 395340',
+            updated_at='2024, 3, 27, 18, 59, 48, 473482'
         )
+
+    def test_owner(self):
+        field = self.tr_account._meta.get_field('owner')
+        self.assertEqual(field.verbose_name, 'owner')
+        self.assertEqual(self.tr_account.owner, self.user)
     
-    def test_identifier_field(self):
-        identifier = self.trading_account._meta.get_field('identifier')
-        self.assertEqual(identifier.verbose_name, 'identifier')
-        self.assertEqual(identifier.max_length, 25)
+    def test_identifier(self):
+        field = self.tr_account._meta.get_field('identifier')
+        self.assertEqual(field.verbose_name, 'identifier')
+        self.assertEqual(field.max_length, 25)
+        self.assertEqual(self.tr_account.identifier, 'testidentifier')
+
+    def test_title(self):
+        field = self.tr_account._meta.get_field('title')
+        self.assertEqual(field.verbose_name, 'title')
+        self.assertEqual(self.tr_account.title, 'Test Title')
+        self.assertEqual(field.max_length, 100)
+
+    def test_description(self):
+        field = self.tr_account._meta.get_field('description')
+        self.assertEqual(field.verbose_name, 'description')
+        self.assertEqual(self.tr_account.description, 'Test Description')
+    
+    def test_type(self):
+        field = self.tr_account._meta.get_field('type')
+        self.assertEqual(field.verbose_name, 'type')
+        self.assertEqual(field.max_length, 4)
+        self.assertEqual(self.tr_account.type, 'REAL')
+
+    def test_initial_balance(self):
+        field = self.tr_account._meta.get_field('initial_balance')
+        self.assertEqual(field.verbose_name, 'initial balance')
+        self.assertEqual(self.tr_account.initial_balance, 10000)
+
+    def test_active(self):
+        field = self.tr_account._meta.get_field('active')
+        self.assertEqual(field.verbose_name, 'active')
+        self.assertTrue(self.tr_account.active)
+    
+    def test_created_and_updated_dates(self):
+        created = self.tr_account._meta.get_field('created_at')
+        self.assertEqual(created.verbose_name, 'created at')
+        updated = self.tr_account._meta.get_field('updated_at')
+        self.assertEqual(updated.verbose_name, 'updated at')
+
+    def test_str(self):
+        self.assertEqual(self.tr_account.__str__(), self.tr_account.title)
 
     def test_get_absolute_url(self):
         self.assertEqual(
-            self.trading_account.get_absolute_url(),
-            reverse('accounts_detail', kwargs={'pk': self.trading_account.pk})
+            self.tr_account.get_absolute_url(),
+            reverse('accounts_detail', kwargs={'pk': self.tr_account.pk})
         )
